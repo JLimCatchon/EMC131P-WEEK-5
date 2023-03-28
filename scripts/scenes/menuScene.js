9b5c280e044c0d2d0951b2e6541e1db8b6042403
Jugaire Limcatchon <101692991+JLimCatchon@users.noreply.github.com>
2023-03-28 11:19:32 +0800

    Update menuScene.js

--- scripts/scenes/menuScene.js
+++ scripts/scenes/menuScene.js
@@ -8,9 +8,12 @@ class menuScene extends Phaser.Scene{
         //add exit button
         this.load.image('play','assets/misc/test.png');
         this.load.image('creditsButton','assets/misc/testCredit.png');
+        this.load.image('menuBackground', 'assets/background/bg.png');
+
     }
 
     create(){
+        this.add.image(400, 300, 'menuBackground');
         const playButton = this.add.image(400,300,'play').setScale(0.5);
         playButton.setInteractive();
         playButton.on('pointerdown', () => {this.scene.start('level1')});
